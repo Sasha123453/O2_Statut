d798987cc9b8a6116f5b35d4cd8fb2df2db6547f
TandDA <tanduevdmitriy@gmail.com>
2023-01-26 23:54:27 +0300

    Адаптив в слайдере

--- js/script.js
+++ js/script.js
@@ -7,16 +7,58 @@ $(document).ready(function(){
       slidesToShow: 4,
       prevArrow: $("#firstBookBlockPrevious"),
       nextArrow: $("#firstBookBlockNext"),
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ]
     });
     $("#secondBooksBlocks").slick({ 
       slidesToShow: 4,
       prevArrow: $("#secondBookBlockPrevious"),
       nextArrow: $("#secondBookBlockNext"),
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ]
     });
     $(".newsBlocks").slick({
       slidesToShow: 3,
       prevArrow: $("#newsPrevious"),
       nextArrow: $("#newsNext"),
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ]
       
     });
 });
